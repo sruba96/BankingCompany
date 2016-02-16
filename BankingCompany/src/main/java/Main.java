b335d78b0f91dbdbd1bae4c6a125cd9fdd3cfc40
Paweł Malina <sruba96@gmail.com>
2016-02-16 14:54:02 +0100

    I added in entity Employee: - date (java.util.Date and java.sql.Date) 							- @Transient - ignore fields

--- BankingCompany/src/main/java/Main.java
+++ BankingCompany/src/main/java/Main.java
@@ -1,5 +1,7 @@
 
 
+import java.sql.Date;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -22,6 +24,11 @@ public class Main
 		employee.setFirstName("Paweł");
 		employee.setLastName("Malina");
 		employee.setSalary(20.0);
+		employee.setDate(new Date(new java.util.Date().getTime()));
+		employee.setSqlDate(new Date(new java.util.Date().getTime()));
+		
+		
+		System.out.println(employee.getDate().toString());
 		
 		entityManager.getTransaction().begin();
 		entityManager.persist(employee);
--- BankingCompany/src/main/java/domain/Employee.java
+++ BankingCompany/src/main/java/domain/Employee.java
@@ -1,11 +1,18 @@
 package domain;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.persistence.UniqueConstraint;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.boot.model.relational.Database;
 
 @Entity
 public class Employee
@@ -14,9 +21,43 @@ public class Employee
 	@GeneratedValue(generator = "increment")
 	@GenericGenerator(name = "increment", strategy = "increment")
 	private long id;
+	
+	@Column(name="firstName", length=10, nullable=true, unique=false)
 	private String firstName;
+	
+	@Transient //This field will not add to database
+	private long ignored;
+	
+	@Column
 	private String lastName;
 	private double salary;
+
+	
+	@Temporal(TemporalType.DATE) // Temporal responsible for convert util.Date to sql.Date
+	private Date date;
+	
+	private java.sql.Date sqlDate;
+	
+	
+	public java.sql.Date getSqlDate()
+	{
+		return sqlDate;
+	}
+
+	public void setSqlDate(java.sql.Date sqlDate)
+	{
+		this.sqlDate = sqlDate;
+	}
+
+	public Date getDate()
+	{
+		return date;
+	}
+
+	public void setDate(Date date)
+	{
+		this.date = date;
+	}
 
 	public long getId()
 	{
